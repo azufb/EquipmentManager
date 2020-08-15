e498aa6491f06f488afe9280de68cc234c3622d8
Azusa Okamoto <curiousazunas@gmail.com>
2020-08-15 15:17:09 +0900

    永続化処理

--- equipmentmanager/src/store/index.js
+++ equipmentmanager/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -46,5 +47,7 @@ export default new Vuex.Store({
     plus(store, payload) {
       store.commit('plus', payload)
     }*/
-  }
+  },
+  plugins: [createPersistedState()]
+
 })
